814140a8c529177c9d52189526112594d410dfb4
盧建安 <s8714081@hotmail.com>
2019-01-10 15:14:49 +0800

    20190110地址編碼表、著作編碼表和行政單位等級編碼表的新增功能

--- resources/views/addresscodes/index.blade.php
+++ resources/views/addresscodes/index.blade.php
@@ -5,6 +5,7 @@
     <div class="panel panel-default">
         <div class="panel-heading">地址编码表</div>
         <div class="panel-body">
+            <a href="{{ route('addresscodes.create') }}" class="pull-right btn btn-default">新增</a>
             <div class="panel-body">
                 <address-code-list></address-code-list>
             </div>

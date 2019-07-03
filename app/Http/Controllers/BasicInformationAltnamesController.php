96ccd295da389fb509094d7d925322da01bb8919
jianan <s8714081@hotmail.com>
2019-07-03 15:17:39 +0800

    申請Issue40合併至develop，[眾包用戶]錄入介面權限判斷。 (#44)
    * 20190628API修改與刪除的功能製作,更新的資料比對功能.
    
    * 20190701[眾包用戶]錄入介面權限判斷

--- app/Http/Controllers/BasicInformationAltnamesController.php
+++ app/Http/Controllers/BasicInformationAltnamesController.php
@@ -66,7 +66,7 @@ class BasicInformationAltnamesController extends Controller
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
-        elseif (Auth::user()->is_active != 1){
+        elseif (Auth::user()->is_active != 1 || Auth::user()->is_admin == 2){
             flash('该用户没有权限，请联系管理员 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
@@ -141,7 +141,7 @@ class BasicInformationAltnamesController extends Controller
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
-        elseif (Auth::user()->is_active != 1){
+        elseif (Auth::user()->is_active != 1 || Auth::user()->is_admin == 2){
             flash('该用户没有权限，请联系管理员 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
@@ -172,7 +172,7 @@ class BasicInformationAltnamesController extends Controller
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
-        elseif (Auth::user()->is_active != 1){
+        elseif (Auth::user()->is_active != 1 || Auth::user()->is_admin == 2){
             flash('该用户没有权限，请联系管理员 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
--- app/Http/Controllers/BasicInformationKinshipController.php
+++ app/Http/Controllers/BasicInformationKinshipController.php
@@ -59,7 +59,7 @@ class BasicInformationKinshipController extends Controller
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
-        elseif (Auth::user()->is_active != 1){
+        elseif (Auth::user()->is_active != 1 || Auth::user()->is_admin == 2){
             flash('该用户没有权限，请联系管理员 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
@@ -109,7 +109,7 @@ class BasicInformationKinshipController extends Controller
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
-        elseif (Auth::user()->is_active != 1){
+        elseif (Auth::user()->is_active != 1 || Auth::user()->is_admin == 2){
             flash('该用户没有权限，请联系管理员 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
@@ -131,7 +131,7 @@ class BasicInformationKinshipController extends Controller
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }
-        elseif (Auth::user()->is_active != 1){
+        elseif (Auth::user()->is_active != 1 || Auth::user()->is_admin == 2){
             flash('该用户没有权限，请联系管理员 @ '.Carbon::now(), 'error');
             return redirect()->back();
         }

0d86ecbbc612d88c2843c0b186cf447d6e409cee
jianan <s8714081@hotmail.com>
2020-07-16 15:18:35 +0800

    20200716保留字過濾函式修正 (#96)
    * 20200420根據名稱、存在時間等條件獲取地點API修正
    
    * 20200716保留字過濾函式修正

--- resources/views/biogmains/defense.blade.php
+++ resources/views/biogmains/defense.blade.php
@@ -2,6 +2,8 @@
 //聯合主鍵保留字弱點防禦函式
 function unionPKDef($key) {
     $key = str_replace("/","(slash)",$key);
+    //因為反斜線在php有用途, 兩個反斜線代表一個反斜線.
+    $key = str_replace("\\","(backslash)",$key);
     $key = str_replace("{","(brackets)",$key);
     $key = str_replace("}","(brackets_r)",$key);
     $result = $key;
@@ -11,6 +13,7 @@ function unionPKDef($key) {
 //欄位值解析保留字
 function unionPKDef_decode($key) {
     $key = str_replace("(slash)","/",$key);
+    $key = str_replace("(backslash)","\\",$key);
     $key = str_replace("(brackets)","{",$key);
     $key = str_replace("(brackets_r)","}",$key);
     $result = $key;
@@ -20,6 +23,7 @@ function unionPKDef_decode($key) {
 //解決版型衝突專用，欄位值解析保留字。
 function unionPKDef_decode_for_convert($key) {
     $key = str_replace("(slash)","/",$key);
+    $key = str_replace("(backslash)","\\",$key);
     $key = str_replace("(brackets)(brackets)","{ { ",$key);
     $key = str_replace("(brackets)","{",$key);
     $key = str_replace("(brackets_r)(brackets_r)","} } ",$key);

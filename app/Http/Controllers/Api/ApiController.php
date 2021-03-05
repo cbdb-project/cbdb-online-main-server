791e5d0163d98c611ba84662ce91bd2ab6456035
盧建安 <s8714081@hotmail.com>
2021-03-05 08:48:34 +0800

    20210305API[post_list]增加[pNameChnAlt]輸出

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -96,7 +96,7 @@ class ApiController extends Controller
     }
 
     //20200427依據指定規格製作office_list_by_name API
-    //202010304增列data[i].pNameChnAlt的屬性，官職的別名漢字。
+    //20210304增列data[i].pNameChnAlt的屬性，官職的別名漢字。
     protected function office_list_by_name(Request $request){
         $ans = $data = $data_val = array();
         // 變數接值
@@ -436,6 +436,7 @@ class ApiController extends Controller
     }
 
     //20200324依據指定規格製作API
+    //20210305增列data[i].pNameChnAlt的屬性，官職的別名漢字。
     protected function post_list(Request $request){
         $ans = $data = $data_val = array();
         // 變數接值
@@ -465,6 +466,7 @@ class ApiController extends Controller
                 $data_val['pId'] = $c_office_id;
                 $data_val['pName'] = $val2->c_office_pinyin;
                 $data_val['pNameChn'] = $val2->c_office_chn;
+                $data_val['pNameChnAlt'] = $val2->c_office_chn_alt;
                 array_push($data, $data_val);
             }
         }

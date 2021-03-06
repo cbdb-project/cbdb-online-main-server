7263b8181c24489eb686c50716dbebaf48a16428
Hongsu Wang <sudospace@gmail.com>
2021-03-06 13:10:48 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,5 +5,5 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2017 <a href="https://projects.iq.harvard.edu/cbdb" target="_blank">CBDB</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; <a href="https://projects.iq.harvard.edu/cbdb" target="_blank">CBDB</a>.</strong> Content on this site is licensed under a <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0</a> International License.
 </footer>

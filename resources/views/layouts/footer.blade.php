57e566c853a4a2a20c0ead9034a880951a66e7a5
Hongsu Wang <sudospace@gmail.com>
2021-03-06 13:13:28 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        Anything you want
+        
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; <a href="https://projects.iq.harvard.edu/cbdb" target="_blank">CBDB</a>.</strong> Content on this site is licensed under a <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0</a> International License.

a5698d76a8680a9adb549dac0277c63b037652e5
jianan <s8714081@hotmail.com>
2023-06-30 02:33:54 +0800

    20230629新建人物刪除[姓名(拼音)]輸入欄位，使用自動生成拼音。 (#232)

--- resources/views/biogmains/basicinformation/create.blade.php
+++ resources/views/biogmains/basicinformation/create.blade.php
@@ -15,10 +15,6 @@
                     <label for="c_name_chn">姓名（中）</label>
                     <input type="text" name="c_name_chn" class="form-control" placeholder="姓名（中）" required>
                 </div>
-                <div class="form-group">
-                    <label for="c_name">姓名（拼音）</label>
-                    <input type="text" name="c_name" class="form-control" placeholder="姓名（拼音）">
-                </div>
                 <div class="form-group"><button class="btn btn-success pull-right" type="submit">Submit</button></div>
             </form>
         </div>

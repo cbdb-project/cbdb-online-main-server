0421ec35f180e12763f791f7eab814d3c767e18b
frankslin <frankslin@users.noreply.github.com>
2025-10-21 01:05:21 -0700

    為 `/codes` 引入白名單機制 (#321)
    * 20251018 Update table name check in CodesRepository so we don't show oauth_auth_codes table [#316]
    
    * 20251020 加入 Codes 白名單與後端分頁。
    
    - 新增 config/codes.php，統一管理可用資料表與每頁筆數設定。
    - CodesRepository 優先讀取設定/環境變數，並提供重複使用的 allowedTables() 方法。
    - CodesController 在所有 CRUD 前檢查白名單，避免隨意讀寫其他資料表；列表改用設定檔的 page size。
    
    * Use configured database for codes tables

--- app/Http/Controllers/CodesController.php
+++ app/Http/Controllers/CodesController.php
@@ -16,10 +16,25 @@ class CodesController extends Controller
     protected $codesrepostory;
     protected $operationRepository;
 
+    protected $allowedTables = [];
+    protected $allowedTablesMap = [];
+
     public function __construct(CodesRepository $codesRepository, OperationRepository $operationRepository)
     {
         $this->codesrepostory = $codesRepository;
         $this->operationRepository = $operationRepository;
+        $this->allowedTables = $this->codesrepostory->allowedTables();
+        $map = $this->codesrepostory->allowedTableMap();
+        $this->allowedTablesMap = $map;
+    }
+
+    protected function guardTable(string $table): string
+    {
+        $key = strtoupper($table);
+        if (!isset($this->allowedTablesMap[$key])) {
+            abort(404);
+        }
+        return $this->allowedTablesMap[$key];
     }
 
     public function index()
@@ -34,9 +49,10 @@ class CodesController extends Controller
 
     public function show($table_name)
     {
+        $table = $this->guardTable($table_name);
         try {
             $perPage = config('codes.per_page', 20);
-            $query = DB::table($table_name);
+            $query = DB::table($table);
             $data = $query->paginate($perPage);
 
             $firstRow = $data->first();
@@ -66,14 +82,14 @@ class CodesController extends Controller
                     $thead = array_keys((array) $firstRow);
                 }
             } else {
-                $thead = Schema::getColumnListing($table_name);
+                $thead = Schema::getColumnListing($table);
             }
             return view('codes.show', [
                 'page_title' => 'Codes',
-                'page_description' => $table_name,
+                'page_description' => $table,
                 'page_url' => '/codes',
-                'archer' => "<li class='active'>".htmlspecialchars($table_name, ENT_QUOTES, 'UTF-8')."</li>",
-                'q' => $table_name,
+                'archer' => "<li class='active'>".e($table)."</li>",
+                'q' => $table,
                 'thead' => $thead,
                 'data' => $data,
             ]);
@@ -86,24 +102,25 @@ class CodesController extends Controller
     public function edit($table_name,$id)
     {
 //        dd($table_name);
-        if($table_name){
+        $table = $this->guardTable($table_name);
+        if($table){
             try{
                 //20210323修改聯合主鍵的邏輯
                 $temp = explode("_._", $id);
-                $id_name = $this->getIdName($table_name);
-                $id_name_1 = $this->getIdName_1($table_name);
-                $id_name_2 = $this->getIdName_2($table_name);
+                $id_name = $this->getIdName($table);
+                $id_name_1 = $this->getIdName_1($table);
+                $id_name_2 = $this->getIdName_2($table);
                 //$data = DB::table($table_name)->where($id_name, $id)->first();
-                $data = DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->first();
+                $data = DB::table($table)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->first();
                 //$data = DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->where($id_name_2, $temp[2])->first();
                 //修改結束
                 return view('codes.edit', [
                     'page_title' => 'Codes',
-                    'page_description' => $table_name,
+                    'page_description' => $table,
                     'page_url' => '/codes',
-                    'archer' => "<li><a href='/codes/".$table_name."'>".$table_name."</a></li>",
+                    'archer' => "<li><a href='/codes/".rawurlencode($table)."'>".e($table)."</a></li>",
                     'id' => $id, 'row' => $data,
-                    'table' => $table_name]);
+                    'table' => $table]);
             }catch (\PDOException $e) {
                 flash('找不到该数据表', 'warning');
                 return redirect()->back();
@@ -115,6 +132,7 @@ class CodesController extends Controller
 
     public function update(Request $request, $table_name, $id)
     {
+        $table = $this->guardTable($table_name);
         if (!Auth::check()) {
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
@@ -127,12 +145,12 @@ class CodesController extends Controller
         $data = array_except($data, ['_method', '_token']);
 //        dd($data);
         //20210323修改聯合主鍵的邏輯
-        $id_name = $this->getIdName($table_name);
-        $id_name_1 = $this->getIdName_1($table_name);
-        $id_name_2 = $this->getIdName_2($table_name);
+        $id_name = $this->getIdName($table);
+        $id_name_1 = $this->getIdName_1($table);
+        $id_name_2 = $this->getIdName_2($table);
         $temp = explode("_._", $id);
         //DB::table($table_name)->where($id_name, $id)->update($data);
-        DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->update($data);
+        DB::table($table)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->update($data);
         //DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->where($id_name_2, $temp[2])->update($data);
         //修改結束
         flash('Update success @ '.Carbon::now(), 'success');
@@ -141,32 +159,34 @@ class CodesController extends Controller
         $id = $data[$id_name].'_._'.$data[$id_name_1];
         //$id = $data[$id_name].'_._'.$data[$id_name_1].'_._'.$data[$id_name_2];
         //return redirect()->route('codes.show', ['table_name' => $table_name]);
-        return redirect()->route('codes.edit', ['table_name' => $table_name, 'id' => $id]);
+        return redirect()->route('codes.edit', ['table_name' => $table, 'id' => $id]);
     }
 
     //20210315增加table_name等於SOCIAL_INSTITUTION_CODES的例外判斷式，將預設遮除的第1個欄位呈現。
     public function create($table_name)
     {
 //        dd($table_name);
-        $data = Schema::getColumnListing($table_name);
+        $table = $this->guardTable($table_name);
+        $data = Schema::getColumnListing($table);
         $id_ = $data[0];
         //20210323遮除「第一欄預設隱藏」
         //if($table_name != 'SOCIAL_INSTITUTION_CODES') {
             //$data = array_splice($data, 1);
         //}
-        $id = DB::table($table_name)->max($id_) + 1;
+        $id = DB::table($table)->max($id_) + 1;
         return view('codes.create',[
             'page_title' => 'Codes',
-            'page_description' => $table_name,
+            'page_description' => $table,
             'page_url' => '/codes',
-            'archer' => "<li><a href='/codes/".$table_name."'>".$table_name."</a></li>",
+            'archer' => "<li><a href='/codes/".rawurlencode($table)."'>".e($table)."</a></li>",
             'row' => $data,
-            'id' => $id, 'table' => $table_name]);
+            'id' => $id, 'table' => $table]);
     }
 
     //20210315增加table_name等於SOCIAL_INSTITUTION_CODES的例外判斷式，將預設自動增加的$id遮除。
     public function store(Request $request, $table_name)
     {
+        $table = $this->guardTable($table_name);
         if (!Auth::check()) {
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
@@ -188,19 +208,20 @@ class CodesController extends Controller
             //$id = $data[$id_];
         //}
         //20210323插入聯合主鍵的邏輯
-        $id_name = $this->getIdName($table_name);
-        $id_name_1 = $this->getIdName_1($table_name);
-        $id_name_2 = $this->getIdName_2($table_name);
+        $id_name = $this->getIdName($table);
+        $id_name_1 = $this->getIdName_1($table);
+        $id_name_2 = $this->getIdName_2($table);
         $id = $data[$id_name].'_._'.$data[$id_name_1];
         //$id = $data[$id_name].'_._'.$data[$id_name_1].'_._'.$data[$id_name_2];
         //修改結束
-        DB::table($table_name)->insert($data);
+        DB::table($table)->insert($data);
         flash('Store success @ '.Carbon::now(), 'success');
-        return redirect()->route('codes.edit', ['table_name' => $table_name, 'id' => $id]);
+        return redirect()->route('codes.edit', ['table_name' => $table, 'id' => $id]);
     }
 
     public function destroy($table_name, $id)
     {
+        $table = $this->guardTable($table_name);
         if (!Auth::check()) {
             flash('请登入后编辑 @ '.Carbon::now(), 'error');
             return redirect()->back();
@@ -211,28 +232,28 @@ class CodesController extends Controller
         }
         //20210323修改聯合主鍵的邏輯
         $temp = explode("_._", $id);
-        $id_name = $this->getIdName($table_name);
-        $id_name_1 = $this->getIdName_1($table_name);
-        $id_name_2 = $this->getIdName_2($table_name);
+        $id_name = $this->getIdName($table);
+        $id_name_1 = $this->getIdName_1($table);
+        $id_name_2 = $this->getIdName_2($table);
 
-        $row = DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->first();
+        $row = DB::table($table)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->first();
         //$row = DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->where($id_name_2, $temp[2])->first();
         //修改結束
         $op = [
             'op_type' => 4,
-            'resource' => $table_name,
+            'resource' => $table,
             'resource_id' => $id,
             'resource_data' => json_encode((array)$row)
         ];
         //$this->operationRepository->store($op);
         //20181207建安修改片段
         $row2 = json_encode((array)$row);
-        $this->operationRepository->store(Auth::id(), '', 4, $table_name, $id, $row2);
+        $this->operationRepository->store(Auth::id(), '', 4, $table, $id, $row2);
         //修改結束
-        DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->delete();
+        DB::table($table)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->delete();
         //DB::table($table_name)->where($id_name, $temp[0])->where($id_name_1, $temp[1])->where($id_name_2, $temp[2])->delete();
         flash('Delete success @ '.Carbon::now(), 'success');
-        return redirect()->route('codes.show', ['table_name' => $table_name]);
+        return redirect()->route('codes.show', ['table_name' => $table]);
     }
 
     protected function getIdName($table_name)
--- app/Repositories/CodesRepository.php
+++ app/Repositories/CodesRepository.php
@@ -9,21 +9,84 @@
 namespace App\Repositories;
 
 
-use Illuminate\Support\Facades\DB;
-
 class CodesRepository
 {
+    /**
+     * Get the list of allowed code tables.
+     *
+     * @return array
+     */
+    public function allowedTables(): array
+    {
+        $configTables = config('codes.tables', []);
+        if (!empty($configTables)) {
+            return array_values(array_unique($configTables));
+        }
+
+        $envTables = env('CODES_TABLES');
+        if (!empty($envTables)) {
+            return array_values(array_unique(array_filter(array_map('trim', explode(',', $envTables)))));
+        }
+
+        return [];
+    }
+
+    /**
+     * Return mapping of uppercase table name to actual table name in database.
+     *
+     * @return array<string,string>
+     */
+    public function allowedTableMap(): array
+    {
+        $allowed = $this->allowedTables();
+        if (empty($allowed)) {
+            return [];
+        }
+
+        $map = [];
+        foreach ($allowed as $table) {
+            $map[strtoupper($table)] = $table;
+        }
+
+        try {
+            $defaultConnection = config('database.default');
+            $connectionName = config('codes.connection') ?: $defaultConnection;
+            $connection = \Illuminate\Support\Facades\DB::connection($connectionName);
+
+            $tables = $connection->select('SHOW TABLES');
+            $databaseName = config('codes.database');
+            if (empty($databaseName)) {
+                $databaseName = config("database.connections.{$connectionName}.database");
+            }
+            if (empty($databaseName)) {
+                $databaseName = $connection->getDatabaseName();
+            }
+            $columnName = $databaseName ? "Tables_in_{$databaseName}" : null;
+
+            foreach ($tables as $row) {
+                $rowArray = (array) $row;
+                if ($columnName && isset($rowArray[$columnName])) {
+                    $actual = $rowArray[$columnName];
+                } else {
+                    $actual = array_values($rowArray)[0] ?? null;
+                }
+                if (!$actual) {
+                    continue;
+                }
+                $upper = strtoupper($actual);
+                if (isset($map[$upper])) {
+                    $map[$upper] = $actual;
+                }
+            }
+        } catch (\Exception $e) {
+            // Ignore DB errors; fallback to configured casing.
+        }
+
+        return $map;
+    }
+
     public function codes()
     {
-        $res = array();
-        $tables = DB::select('SHOW TABLES');
-        foreach($tables as $table)
-        {
-            if(str_contains($table->Tables_in_cbdb_data,'_CODES') or str_contains($table->Tables_in_cbdb_data,'_codes')) {
-                array_push($res, $table->Tables_in_cbdb_data);
-            }
-
-        }
-        return $res;
+        return $this->allowedTables();
     }
 }

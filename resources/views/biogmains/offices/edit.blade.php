66799cce2976927e635f6c42dd139c3339136015
Frank Lin <github@linshuang.info>
2025-10-22 21:44:49 -0700

    20251023 官名編輯頁底部的 create 按鈕暫時改為停用狀態

--- resources/views/biogmains/offices/edit.blade.php
+++ resources/views/biogmains/offices/edit.blade.php
@@ -219,8 +219,8 @@
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-default">Submit</button>
-                        <a href="../../../../basicinformation/{{ $row->c_personid }}/offices/{{ $row->c_office_id.'-'.$row->c_posting_id }}/saveas" class="btn btn-success" style="margin-left:40px;">create</a>
+                        <button type="submit" class="btn btn-default" id="office-edit-submit">Submit</button>
+                        <button type="button" class="btn btn-success" style="margin-left:40px;" disabled title="功能暂时关闭以避免误操作">create</button>
                     </div>
                 </div>
             </form>

f926e1aae8ea14bff6874ec7814d21e9afabc631
frankslin <frankslin@users.noreply.github.com>
2025-11-02 19:22:34 -0800

    強化基本信息與檢視表響應式體驗 (#376)
    - 為人名、地址從屬、著作版本列表新增 table-scroll-x 容器，改善手機橫向瀏覽
    
    - 共用樣式集中於 styles.css，包含 overscroll 行為與欄寬限制
    
    - 檢視表頁面套用新樣式，確保大量欄位時維持可讀性並避免滑動觸發瀏覽器手勢

--- resources/assets/js/components/NameList.vue
+++ resources/assets/js/components/NameList.vue
@@ -11,6 +11,7 @@
                 </div>
             </div>
         </div>
+        <div class="table-responsive table-scroll-x">
         <table class="table table-hover table-condensed">
             <caption>共計{{names.total}}條記錄</caption>
             <thead>
@@ -38,6 +39,7 @@
                 </tr>
             </tbody>
         </table>
+        </div>
         <nav class="pull-right" aria-label="Page navigation">
             <ul class="pagination">
                 <li v-if="showFirst"><a href="javascript:" @click="current_page--">«</a></li>

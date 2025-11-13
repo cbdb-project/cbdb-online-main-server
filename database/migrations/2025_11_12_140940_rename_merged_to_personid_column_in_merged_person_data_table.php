b64fe7aae8c9d460b90df02766927584c56b4425
Frank Lin <github@linshuang.info>
2025-11-12 20:01:18 -0800

    新增資料庫遷移：重新命名 merged_person_data 表中的 merged 欄位為 c_personid

---
+++ database/migrations/2025_11_12_140940_rename_merged_to_personid_column_in_merged_person_data_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameMergedToPersonidColumnInMergedPersonDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('MERGED_PERSON_DATA', function (Blueprint $table) {
+            $table->renameColumn('c_merged_to_personid', 'c_merged_from_personid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('MERGED_PERSON_DATA', function (Blueprint $table) {
+            $table->renameColumn('c_merged_from_personid', 'c_merged_to_personid');
+        });
+    }
+}

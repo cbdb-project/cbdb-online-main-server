9abdfc142ec10285f2ed9e90b8b4c7cc6a9781a0
Frank Lin <github@linshuang.info>
2025-11-16 21:36:34 -0800

    Revert "Upgrade Laravel to 5.8 (#453)"
    This reverts commit 2572afd44f8deabfb184a9065b0da8dad2845cac.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,21 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function setUp(): void
-    {
-        $compiledPath = dirname(__DIR__) . '/storage/framework/testing-views';
-        if (!is_dir($compiledPath)) {
-            mkdir($compiledPath, 0777, true);
-        }
-
-        // Ensure Laravel picks up the custom compiled path before the application boots.
-        putenv('VIEW_COMPILED_PATH=' . $compiledPath);
-        $_ENV['VIEW_COMPILED_PATH'] = $compiledPath;
-        $_SERVER['VIEW_COMPILED_PATH'] = $compiledPath;
-
-        parent::setUp();
-
-        config(['view.compiled' => $compiledPath]);
-    }
 }

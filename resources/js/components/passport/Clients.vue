7215e041fc77e3e73e1880490a91baabd3adb5c5
Frank Lin <github@linshuang.info>
2025-12-14 12:27:32 -0800

    fix: update the modal dialog close button location in Passport Vue components

--- resources/js/components/passport/Clients.vue
+++ resources/js/components/passport/Clients.vue
@@ -81,11 +81,10 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-
                         <h4 class="modal-title">
                             Create Client
                         </h4>
+                        <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
 
                     <div class="modal-body">
@@ -149,11 +148,10 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-
                         <h4 class="modal-title">
                             Edit Client
                         </h4>
+                        <button type="button " class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
 
                     <div class="modal-body">
